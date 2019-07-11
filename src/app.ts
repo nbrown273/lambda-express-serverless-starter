9fa17a4a7a1ff3ddfceb1129b0a84789606c84a8
Rommel Villagomez <rommelvillagomez@Rommels-MacBook-Air.local>
2019-07-10 19:56:56 -0500

    completed ex1

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import express from 'express';
 import cors from 'cors'
+import shortid from 'shortid';
+import uuidv4 from 'uuid/v4';
+import generator from 'generate-password';
 
 const app = express();
 app.use(cors());
 
 app.get('/', function (req, res) {
-  res.send('hello world!')
+  res.send('hello world');
 });
 
+app.get('/shortid', function (req, res) {
+  res.send(shortid.generate());
+});
+
+app.get('/uuid', function (req, res) {
+  res.send(uuidv4())
+});
+
+app.get('/password', function (req, res) {
+  res.send(generator.generate({
+    length: 10,
+    numbers: true
+  }))
+});
 export default app;
