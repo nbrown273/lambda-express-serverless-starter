41549983f21d77869850240baaf47aec5abbbc38
Rommel Villagomez <rommelvillagomez@Rommels-MacBook-Air.local>
2019-07-24 00:43:00 -0500

    add route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.get('/form', function (req, res) {
+  const contents = fs.readFileSync(`./public/index1.html`);
+  res.send(contents.toString())
+});
+
 app.get('/', function (req, res) {
   res.send({ message: 'hello world!' })
 });
