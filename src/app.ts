a6923e8ac87a198291708185c6e043327664fb57
Rommel Villagomez <rommelvillagomez@Rommels-MacBook-Air.local>
2019-07-23 21:27:08 -0500

    send json back

--- src/app.ts
+++ src/app.ts
@@ -8,21 +8,22 @@ const app = express();
 app.use(cors());
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send({message: 'hello world'});
 });
 
 app.get('/shortid', function (req, res) {
-  res.send(shortid.generate());
+  res.send({shortid: shortid.generate()});
 });
 
 app.get('/uuid', function (req, res) {
-  res.send(uuidv4())
+  res.send({uuid: uuidv4()})
 });
 
 app.get('/password', function (req, res) {
-  res.send(generator.generate({
+  res.send({password: generator.generate({
     length: 10,
     numbers: true
-  }))
+  })})
 });
+
 export default app;
