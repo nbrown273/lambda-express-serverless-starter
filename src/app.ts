be499c6345decb46d726440b3b88b43a1dfda51c
Rommel Villagomez <rommelvillagomez@Rommels-MacBook-Air.local>
2019-07-24 00:50:49 -0500

    clean it up

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+import cors from 'cors'
+const fs = require('fs');
 
 const app = express();
+app.use(cors());
 
 app.get('/form', function (req, res) {
-  const contents = fs.readFileSync(`./public/index1.html`);
+  const contents = fs.readFileSync(`./public/index.html`);
   res.send(contents.toString())
 });
 
 app.get('/', function (req, res) {
-  res.send({ message: 'hello world!' })
-});
+  res.send({ message: 'hello world!' })});
 
 export default app;
